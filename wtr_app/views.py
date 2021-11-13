89c247d3a2fb6f0e56d6ddf13a5380a67ff974d3
Khailas R <khailas303@gmail.com>
2021-11-13 06:08:13 +0530

    bug fix

--- wtr_app/views.py
+++ wtr_app/views.py
@@ -13,12 +13,12 @@ api_key = os.getenv('WEATHER_API_KEY')
 def index(request, *args, **kwargs):
     if request.method == 'POST':
         city = request.POST['city']
-        source = urllib.request.urlopen(f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=meteric&appid={api_key}').read()
+        source = urllib.request.urlopen(f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}').read()
         
         list_of_data = json.loads(source)
         context = {
             'country_code': str(list_of_data['sys']['country']),
-            'coordinate': str(list_of_data['coord']['lon']) + ', ' + str(list_of_data['coor']['lat']),
+            'coordinate': str(list_of_data['coord']['lon']) + ', ' + str(list_of_data['coord']['lat']),
             'temp': str(list_of_data['main']['temp']) + ' °C',
             'pressure': str(list_of_data['main']['pressure']),
             'humidity': str(list_of_data['main']['humidity']),
